3f90bd51df20e2173eb0afefa534e730ae506b91
Adelaide Stucchi <94860246+adelplasters@users.noreply.github.com>
2022-10-05 15:50:11 +0200

    Add files via upload

--- mixed_model/link_efficiency.m
+++ mixed_model/link_efficiency.m
@@ -1,7 +1,20 @@
 %link efficiency calculation 
 
-function [link_eff] = link_efficiency(k,Tx,Rx)
+function [link_eff] = link_efficiency(k,Tx,Rx,caso)
 
-    link_eff = (k^2)*Tx.Q*Rx.Q/((1 + sqrt(1 + (k^2)*Tx.Q*Rx.Q)))^2;
+        link_eff = zeros(100);
+
+        switch caso
+            case 'coaxial'
+
+            for i=1:100
+                link_eff(i) = (k(i)^2)*Tx.Q*Rx.Q/((1 + sqrt(1 + (k(i)^2)*Tx.Q*Rx.Q)))^2;
+            end 
+
+            case 'lateral'
+                 link_eff = (k.^2)*Tx.Q*Rx.Q./((1 + sqrt(1 + (k.^2)*Tx.Q*Rx.Q))).^2;
+        end 
+
+
 
 end 
